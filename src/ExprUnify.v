1d9bbbb2c367d79487ae09193971b7b835261cfd
Gregory Malecha <gmalecha@cs.harvard.edu>
2013-08-06 22:51:26 -0400

    moving ExprUnify and a few edits to Subst.

--- src/ExprUnify.v
+++ src/ExprUnify.v
@@ -1,4 +1,4 @@
-Require Import Expr.
+Require Import MirrorCore.ExprI.
 Require Import Subst.
 Require Import Prover.
 
@@ -6,13 +6,13 @@ Set Implicit Arguments.
 Set Strict Implicit.
 
 Definition unifier  (ts : types) : Type :=
-  forall (S : Type) (Subst_S : Subst ts S) (P : ProverT ts), 
+  forall (S : Type) (Subst_S : Subst ts S) (P : ProverT ts),
     Facts P ->
     S -> expr ts -> expr ts -> option S.
 
 Definition unifier_WellTyped (ts : types) (fs : tfunctions) (unify : unifier ts) : Prop :=
-  forall  (S : Type) (Subst_S : Subst ts S) (P : ProverT ts) 
-    (F : Facts P) (subst : S) (e1 e2 : expr ts) subst', 
+  forall  (S : Type) (Subst_S : Subst ts S) (P : ProverT ts)
+    (F : Facts P) (subst : S) (e1 e2 : expr ts) subst',
     unify _ _ P F subst e1 e2 = Some subst' ->
     forall (uenv venv : tenv) (t : typ),
       WellTyped_expr fs uenv venv e1 t ->
@@ -24,5 +24,5 @@ Definition unifier_WellTyped (ts : types) (fs : tfunctions) (unify : unifier ts)
       True.
 
 Definition unifier_sound (ts : types) (fs : functions ts) (unify : unifier ts) : Prop :=
-  forall (S : Type) (Subst_S : Subst ts S) (P : ProverT ts) (PC : ProverT_correct P fs) (F : Facts P) (subst : S) (e1 e2 : expr ts) subst', 
+  forall (S : Type) (Subst_S : Subst ts S) (P : ProverT ts) (PC : ProverT_correct P fs) (F : Facts P) (subst : S) (e1 e2 : expr ts) subst',
     unify _ _ P F subst e1 e2 = Some subst' -> True.
--- src/Subst.v
+++ src/Subst.v
@@ -8,14 +8,14 @@ Section subst.
 
   Class Subst :=
   { set : uvar -> expr ts -> T -> option T
-  ; lookup : uvar -> T -> option (expr ts) 
+  ; lookup : uvar -> T -> option (expr ts)
   }.
 
   Section instantiate.
     Variable Subst_T : Subst.
 
     Variable subst : T.
-    
+
     Fixpoint exprInstantiate (l : nat) (e : expr ts) : expr ts :=
       match e with
         | Const _ _ => e
@@ -23,7 +23,7 @@ Section subst.
         | Func _ _ => e
         | App e es => App (exprInstantiate l e) (map (exprInstantiate l) es)
         | Abs t e => Abs t (exprInstantiate (S l) e)
-        | UVar u => 
+        | UVar u =>
           match lookup u subst with
             | None => e
             | Some e' => lift 0 l e'
@@ -35,7 +35,7 @@ Section subst.
 
 (*
 
-  Class SubstOk := 
+  Class SubstOk :=
   { Subst_WellTyped : tfunctions -> tenv -> tenv -> T -> Prop
   ; Subst_Extends : relation T
   ; PreOrder_Subst_Extends : PreOrder Subst_Extends
