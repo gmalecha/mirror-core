a02f4961fc1f446c06c4183144b4b04dd71a5f36
Gregory Malecha <gmalecha@cs.harvard.edu>
2014-09-09 17:48:52 -0400

    instantiate and simplify.

--- theories/STac/Instantiate.v
+++ theories/STac/Instantiate.v
@@ -8,11 +8,9 @@ Require Import MirrorCore.Util.ListMapT.
 Require Import MirrorCore.EnvI.
 Require Import MirrorCore.SymI.
 Require Import MirrorCore.SubstI.
-Require Import MirrorCore.Lemma.
-Require Import MirrorCore.LemmaApply.
 Require Import MirrorCore.InstantiateI.
 Require Import MirrorCore.STac.Core.
-Require Import MirrorCore.STac.Continuation.
+Require Import MirrorCore.STac.Simplify.
 
 Set Implicit Arguments.
 Set Strict Implicit.
@@ -25,25 +23,16 @@ Section parameterized.
   Variable Typ0_Prop : Typ0 _ Prop.
   Let tyProp : typ := @typ0 _ _ _ _.
 
-  Variable vars_to_uvars : nat -> nat -> expr -> expr.
-  Variable exprUnify : tenv typ -> tenv typ -> nat -> expr -> expr -> typ -> subst -> option subst.
   Variable instantiate : (nat -> option expr) -> nat -> expr -> expr.
 
   Section instantiate.
     Variable Subst_subst : Subst subst expr.
     Variable SU : SubstUpdate subst expr.
 
-    (** Think of this like:
-     **   apply lem ; cont
-     ** i.e. first apply the lemma and then require that all side-conditions
-     ** except the last are solved by the prover [tac], currently with
-     ** empty facts.
-     **)
     Definition INSTANTIATE
     : stac typ expr subst :=
-      fun tus tvs sub hs e =>
-        let inst := instantiate (fun u => lookup u sub) 0 in
-        More nil nil sub (List.map inst hs) (inst e).
+      @SIMPLIFY typ expr subst
+                (fun tus tvs sub => instantiate (fun u => lookup u sub) 0).
   End instantiate.
 
   Variable Expr_expr : Expr RType_typ expr.
@@ -56,34 +45,21 @@ Section parameterized.
   Theorem INSTANTIATE_sound
   : stac_sound (INSTANTIATE _).
   Proof.
-    intros. red; simpl.
-    intuition.
+    eapply SIMPLIFY_sound.
+    intros. unfold propD.
     forward.
-    unfold stateD in *.
-    Require Import MirrorCore.Util.Forwardy.
-    forwardy.
+    eapply sem_preserves_if_substD in H3; eauto.
+    red in exprD'_instantiate.
+    eapply exprD'_instantiate with (tvs' := nil) in H1; eauto.
+    simpl in H1.
+    forward_reason.
+    subst. rewrite H1.
+    intros.
     inv_all; subst.
-    erewrite substD_conv
-        with (pfu := app_nil_r_trans _) (pfv := app_nil_r_trans _) in H2.
-    unfold ResType in H2.
-    autorewrite with eq_rw in H2.
-    forwardy. inv_all; subst.
-    eapply sem_preserves_if_substD in H2; eauto.
-    specialize (fun g gD =>
-                  @exprD'_instantiate (tus ++ nil) (tvs ++ nil)
-                                      (fun u : nat => lookup u s) g nil tyProp gD _ H2).
-    Opaque mapT.
-    unfold propD in *.
-    forwardy; inv_all; subst.
-    rewrite exprD'_conv
-       with (pfu := app_nil_r_trans _) (pfv := app_nil_r_trans _) in H0.
-    unfold ResType in H0; autorewrite with eq_rw in H0.
-    forwardy.
-    eapply exprD'_instantiate in H0.
-    simpl in *; forward_reason.
-    change_rewrite H0.
-    inv_all; subst.
-    admit.
+    autorewrite with eq_rw.
+    specialize (H4 us vs Hnil).
+    simpl in *.
+    rewrite H4; auto. reflexivity.
   Qed.
 
 End parameterized.
--- theories/STac/Simplify.v
+++ theories/STac/Simplify.v
@@ -15,6 +15,14 @@ Section parameterized.
     fun tus tvs s hs e =>
       More nil nil s hs (f tus tvs s e).
 
+(*
+  Definition SIMPLIFY_hyps
+             (f : list typ -> list typ -> subst -> expr -> expr)
+  : stac typ expr subst :=
+    fun tus tvs s hs e =>
+      More nil nil s (List.map (f tus tvs s) hs) e.
+*)
+
   Variable RType_typ : RType typ.
   Variable Expr_expr : Expr RType_typ expr.
   Variable ExprOk_expr : ExprOk Expr_expr.
@@ -37,7 +45,7 @@ Section parameterized.
            | Some eD , Some sD , Some eD' =>
              forall us vs,
                sD us vs ->
-               eD us vs <-> eD' us vs
+               (eD us vs <-> eD' us vs)
          end) ->
       stac_sound (SIMPLIFY f).
   Proof.
@@ -75,5 +83,82 @@ Section parameterized.
       destruct (eq_sym (HList.app_nil_r_trans tvs)).
       rewrite H1. congruence. }
   Qed.
+
+(*
+  Theorem SIMPLIFY_hyps_sound
+  : forall f,
+      (forall tus tvs s e e',
+         WellFormed_subst s ->
+         f tus tvs s e = e' ->
+         match @propD _ _ _ Expr_expr _ tus tvs e
+             , @substD _ _ _ _ Expr_expr _ _ tus tvs s
+             , @propD _ _ _ _ _ tus tvs e'
+         with
+           | None , _ , _ => True
+           | _ , None , _ => True
+           | Some _ , Some _ , None => False
+           | Some eD , Some sD , Some eD' =>
+             forall us vs,
+               sD us vs ->
+               (eD us vs <-> eD' us vs)
+         end) ->
+      stac_sound (SIMPLIFY_hyps f).
+  Proof.
+    intros. unfold stac_sound, SIMPLIFY_hyps.
+    intros.
+    specialize (fun g => H tus tvs s g _ H0 eq_refl).
+    forward.
+    split; auto. clear H0.
+    unfold stateD in *.
+    forward.
+    rewrite propD_conv
+       with (pfu := eq_sym (HList.app_nil_r_trans _))
+            (pfv := eq_sym (HList.app_nil_r_trans _)).
+    rewrite substD_conv
+       with (pfu := eq_sym (HList.app_nil_r_trans _))
+            (pfv := eq_sym (HList.app_nil_r_trans _)).
+    autorewrite with eq_rw.
+    rewrite H0.
+    Opaque Traversable.mapT.
+    rewrite ListMapT.mapT_map.
+    match goal with
+      | |- match match ?X with _ => _ end with _ => _ end =>
+        assert (exists y,
+                  X = Some y /\
+                  forall (us : HList.hlist (typD nil) tus)
+                         (vs : HList.hlist (typD nil) tvs),
+                    P1 us vs ->
+                    Forall2 (fun l r => l us vs <-> r us vs) l y)
+    end.
+
+    match goal with
+      | |- match ?X with _ => _ end =>
+        consider X; intros
+    end.
+    rewrite
+    { forward_reason.
+      forward. inv_all; subst.
+      rewrite (HList.hlist_eta x) in *.
+      rewrite (HList.hlist_eta x0) in *.
+      do 2 rewrite HList.hlist_app_nil_r in H6.
+      destruct (eq_sym (HList.app_nil_r_trans tus)).
+      destruct (eq_sym (HList.app_nil_r_trans tvs)).
+      rewrite H2 in *. rewrite H8 in *. rewrite H7 in *.
+      inv_all; subst.
+      intuition. apply H4; auto. }
+    { revert H5.
+      rewrite propD_conv
+         with (pfu := eq_sym (HList.app_nil_r_trans tus))
+              (pfv := eq_sym (HList.app_nil_r_trans tvs)).
+      rewrite substD_conv
+         with (pfu := eq_sym (HList.app_nil_r_trans tus))
+              (pfv := eq_sym (HList.app_nil_r_trans tvs)).
+      autorewrite with eq_rw.
+      Cases.rewrite_all_goal.
+      destruct (eq_sym (HList.app_nil_r_trans tus)).
+      destruct (eq_sym (HList.app_nil_r_trans tvs)).
+      rewrite H1. congruence. }
+  Qed.
+*)
 
 End parameterized.
