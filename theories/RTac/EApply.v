ea36770733949859206d588bfef6f8f2243f27ae
Gregory Malecha <gmalecha@cs.harvard.edu>
2014-09-18 15:22:59 -0400

    this one looks more correct.

--- theories/RTac/EApply.v
+++ theories/RTac/EApply.v
@@ -47,7 +47,11 @@ Section parameterized.
         | Some sub' =>
           let len_uvars := length tus in
           let premises := map (fun x => GGoal (vars_to_uvars 0 len_uvars x)) lem.(premises) in
-          more_list instantiate (fold_left (@CEx _ _) lem.(vars) CTop) sub' premises
+          reduceGoal instantiate (fold_left (@CEx _ _) lem.(vars) CTop) sub'
+                     match premises with
+                       | nil => GSolved
+                       | _ :: _ => GConj premises
+                     end (countUVars ctx + len_vars) (countVars ctx)
       end.
 
 End parameterized.
