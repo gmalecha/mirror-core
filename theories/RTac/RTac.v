353de1e81c537fc3ffeacc862be066bbafd577ca
Gregory Malecha <gmalecha@cs.harvard.edu>
2014-12-02 23:33:33 -0500

    a way to combine two tactic continuations.

--- theories/RTac/RTac.v
+++ theories/RTac/RTac.v
@@ -15,7 +15,9 @@ Require Export MirrorCore.RTac.Apply.
 Require Export MirrorCore.RTac.EApply.
 Require Export MirrorCore.RTac.Simplify.
 Require Export MirrorCore.RTac.Instantiate.
+
 Require Export MirrorCore.RTac.RunOnGoals.
+Require Export MirrorCore.RTac.ThenK.
 Require Export MirrorCore.RTac.Minify.
 (*Require Export MirrorCore.RTac.RunSTac.*)
 Require Export MirrorCore.RTac.AtGoal.
