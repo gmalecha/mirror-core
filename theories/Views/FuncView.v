65ce5107c33bf38bb21b2045de87ebe340669c41
Gregory Malecha <gmalecha@cs.harvard.edu>
2015-07-19 18:07:31 -0700

    cleaning up SymOneOf, still admits in there though.

--- theories/Views/FuncView.v
+++ theories/Views/FuncView.v
@@ -1,7 +1,10 @@
 Require Import ExtLib.Data.Map.FMapPositive.
 Require Import ExtLib.Data.SumN.
 Require Import ExtLib.Data.Positive.
+Require Import ExtLib.Tactics.
 Require Import MirrorCore.Views.Ptrns.
+Require Import MirrorCore.TypesI.
+Require Import MirrorCore.SymI.
 
 Set Implicit Arguments.
 Set Strict Implicit.
@@ -17,9 +20,80 @@ Section FuncView.
 
   Variable FV : FuncView.
 
+  Variable typ : Type.
+  Variable RType_typ : RType typ.
+  Variable Sym_func : RSym func.
+  Variable Sym_A : RSym A.
+
+  Check symAs.
+
   Class FuncViewOk : Type :=
   { fv_ok : forall f a, f_view f = Some a <-> f_insert a = f
+  ; fv_compat : forall (a : A) t,
+      symAs a t = symAs (f_insert a) t
   }.
+
+  Variable RTypeOk_typ : RTypeOk.
+
+  Theorem fv_compat_typ (FVO : FuncViewOk)
+  : forall a, typeof_sym (f_insert a) = typeof_sym a.
+  Proof.
+    intros.
+    generalize (fv_compat a).
+    unfold symAs. intros.
+    generalize dependent (symD a).
+    generalize dependent (symD (f_insert a)).
+    destruct (typeof_sym a).
+    { destruct (typeof_sym (f_insert a)).
+      { intros.
+        specialize (H t).
+        rewrite type_cast_refl in H; eauto.
+        destruct (type_cast t t0); try solve [ inversion H ].
+        inversion H. subst.
+        f_equal. symmetry. assumption. }
+      { intros.
+        specialize (H t).
+        rewrite type_cast_refl in H; eauto.
+        inversion H. } }
+    { intros.
+      destruct (typeof_sym (f_insert a)); auto.
+      specialize (H t).
+      rewrite type_cast_refl in H. inversion H.
+      eauto. }
+  Defined.
+
+  Theorem fv_compat_val (FVO : FuncViewOk)
+  : forall a,
+        symD a = match fv_compat_typ _ a in _ = T return match T with
+                                                         | Some t => typD t
+                                                         | None => unit:Type
+                                                         end
+                 with
+                 | eq_refl => symD (f_insert a)
+                 end.
+  Proof.
+    intros.
+    assert (typeof_sym a = None \/ exists t, typeof_sym a = Some t).
+    { clear. destruct (typeof_sym a); eauto. }
+    destruct H.
+    { generalize (symD a).
+      generalize (symD (f_insert a)).
+      generalize (fv_compat_typ FVO a).
+      rewrite H.
+      intro. rewrite e.
+      clear. destruct y; destruct y; reflexivity. }
+    { destruct H.
+      generalize (fv_compat a x).
+      unfold symAs.
+      generalize dependent (symD a).
+      generalize dependent (symD (f_insert a)).
+      generalize (fv_compat_typ FVO a).
+      destruct e.
+      rewrite H.
+      setoid_rewrite type_cast_refl; eauto.
+      intros.
+      inv_all. assumption. }
+  Qed.
 
   Definition ptrn_view {T} (p : ptrn A T) : ptrn func T :=
     fun e _T good bad =>
@@ -66,51 +140,69 @@ Section FuncViewSumN.
   Context {A func : Type}.
 
   Global Instance FuncViewPMap (p : positive) (m : FMapPositive.pmap Type)
-	 (pf : Some A = pmap_lookup' m p) :
-    FuncView (OneOf m) A :=
-    {
-      f_insert f := Into f p pf;
-      f_view f :=
-	match f with
-	  | Build_OneOf _ p' pf1 =>
-	    match Pos.eq_dec p p' with
-	      | left Heq =>
-	       Some (eq_rect_r (fun o => match o with
-                                           | Some T => T
-                                           | None => Empty_set
-                                         end) pf1
-	                       (eq_rect _ (fun p => Some A = pmap_lookup' m p) pf _ Heq))
-	      | right _ => None
-	    end
+	 (pf : Some A = pmap_lookup' m p)
+  : FuncView (OneOf m) A :=
+  { f_insert f := Into f p pf
+  ; f_view f :=
+      match f with
+      | Build_OneOf _ p' pf1 =>
+	match Pos.eq_dec p p' with
+	| left Heq =>
+	  Some (eq_rect_r (fun o => match o with
+                                    | Some T => T
+                                    | None => Empty_set
+                                    end) pf1
+	                  (eq_rect _ (fun p => Some A = pmap_lookup' m p) pf _ Heq))
+	| right _ => None
 	end
+      end
+  }.
 
-    }.
+  Variable typ : Type.
+  Variable RType_typ : RType typ.
 
-  Global Instance FuncViewOkPMap (p : positive) (m : FMapPositive.pmap Type)
-	 (pf : Some A = pmap_lookup' m p) :
-    FuncViewOk (FuncViewPMap p m pf) := _.
+  SearchAbout RSym OneOf.
+  Require Import MirrorCore.syms.SymOneOf.
+
+  Print SymOneOf.
+
+  Global Instance FuncViewOkPMap
+         (p : positive) (m : FMapPositive.pmap Type)
+         (syms : forall p : positive,
+             RSym match pmap_lookup' m p with
+                  | Some T => T
+                  | None => Empty_set
+                  end)
+	 (pf : Some A = pmap_lookup' m p)
+         (RSa : RSym A)
+  : FuncViewOk (FuncViewPMap p m pf) (RSymOneOf _ _) RSa.
   Proof.
     constructor.
-    simpl. destruct f.
-    destruct (Pos.eq_dec p index).
-    { subst. split; intros.
-      { unfold Into. f_equal.
-        inversion H; clear H; subst.
-        destruct pf. reflexivity. }
-      { f_equal.
-        unfold Into in H.
-        cutrewrite (value = match
-                 pf in (_ = z)
-                 return match z with
-                        | Some T => T
-                        | None => Empty_set
-                        end
-               with
-               | eq_refl => a
-               end).
-        { clear. simpl. destruct pf. reflexivity. }
-        clear - H.
-        admit. } }
+    { simpl. destruct f.
+      destruct (Pos.eq_dec p index).
+      { subst. split; intros.
+        { unfold Into. f_equal.
+          inversion H; clear H; subst.
+          destruct pf. reflexivity. }
+        { f_equal.
+          unfold Into in H.
+          cutrewrite (value = match
+                        pf in (_ = z)
+                        return match z with
+                               | Some T => T
+                               | None => Empty_set
+                               end
+                      with
+                      | eq_refl => a
+                      end).
+          { clear. simpl. destruct pf. reflexivity. }
+          clear - H.
+          admit. } }
+      { split.
+        { inversion 1. }
+        { intros. unfold Into in H.
+          admit. } } }
+    { admit. }
   Admitted.
 
 End FuncViewSumN.
---
+++ theories/syms/SymOneOf.v
@@ -0,0 +1,198 @@
+Require Import ExtLib.Core.RelDec.
+Require Import ExtLib.Data.Map.FMapPositive.
+Require Import ExtLib.Data.SumN.
+
+Require Import MirrorCore.SymI.
+Require Import MirrorCore.TypesI.
+
+Set Implicit Arguments.
+Set Strict Implicit.
+
+Section RSym_OneOf.
+  Context {typ : Type} {RType_typ : RType typ}.
+
+  Instance RSym_Empty_set : RSym (Empty_set) :=
+  { typeof_sym s := None
+  ; symD := fun f => match f return unit with end
+  ; sym_eqb := fun f _ => match f return option bool with end
+  }.
+
+  Definition typeof_sym_OneOf {m : pmap Type}
+    (H : forall p, RSym (match pmap_lookup' m p with | Some T => T | None => Empty_set end))
+    (s : OneOf m) : option typ :=
+    match s with
+      | Build_OneOf _ x v =>
+        match pmap_lookup' m x as o return o = pmap_lookup' m x -> option typ with
+          | Some T =>
+            fun pf =>
+              let RSym_p :=
+                  eq_rect_r (fun o => RSym (match o with
+                                              | Some T => T
+                                              | None => Empty_set
+                                            end)) (H x) pf in
+              let v' :=
+                  eq_rect_r (fun o => match o with
+                                        | Some T => T
+                                        | None => Empty_set
+                                      end) v pf in
+              typeof_sym (RSym := RSym_p) v'
+          | None => fun _ => None
+        end eq_refl
+    end.
+
+
+  Definition symD_OneOf (m : pmap Type)
+    (H : forall p, RSym (match pmap_lookup' m p with
+                         | Some T => T
+                         | None => Empty_set
+                         end))
+    (f : OneOf m)
+  : match typeof_sym_OneOf H f with
+    | Some t => typD t
+    | None => unit:Type
+    end.
+  refine (
+    match f as f' return f = f' ->
+                         match typeof_sym_OneOf H f' with
+                           | Some t => typD t
+                           | None => unit:Type
+                         end with
+      | Build_OneOf _ x v =>
+        fun eqf =>
+          match pmap_lookup' m x as o
+                return o = pmap_lookup' m x ->
+                       match typeof_sym_OneOf H {| SumN.index := x; value := v |} with
+                         | Some t => typD t
+                         | None => unit:Type
+                       end with
+            | Some T =>
+              fun pf =>
+                let RSym_p :=
+                    eq_rect_r (fun o => RSym (match o with
+                                                | Some T => T
+                                                | None => Empty_set
+                                              end)) (H x) pf in
+                let v' :=
+                    eq_rect_r (fun o => match o with
+                                          | Some T => T
+                                          | None => Empty_set
+                                        end) v pf in
+              let v'' := symD (RSym := RSym_p) v' in _
+            | None =>
+              fun pf =>
+                let RSym_p :=
+                    eq_rect_r (fun o => RSym (match o with
+                                                | Some T => T
+                                                | None => Empty_set
+                                              end)) (H x) pf in
+                let v' :=
+                    eq_rect_r (fun o => match o with
+                                          | Some T => T
+                                          | None => Empty_set
+                                        end) v pf in _
+          end eq_refl
+    end eq_refl).
+  simpl.
+  generalize dependent (H x).
+  clear H.
+  clear eqf.
+  revert v v'.
+  rewrite <- pf.
+  intros.
+  apply v''.
+
+  intros; simpl.
+  generalize dependent (H x).
+  clear H eqf.
+  revert v v'.
+  rewrite <- pf.
+  intros.
+  apply tt.
+
+  Defined.
+
+  Definition sym_eqb_OneOf
+    (m : pmap Type)
+    (H : forall p, RSym (match pmap_lookup' m p with
+                         | Some T => T
+                         | None => Empty_set
+                         end))
+    (f1 f2 : OneOf m) : option bool :=
+    match Pos.eq_dec (index _ f1) (index _ f2) with
+    | left pf =>
+      match pf in _ = T
+            return RSym (match pmap_lookup' m T with
+                         | Some T => T
+                         | None => Empty_set
+                         end) ->
+                   match pmap_lookup' m (index _ f1) with
+                   | Some T => T
+                   | None => Empty_set
+                   end ->
+                   match pmap_lookup' _ T with
+                   | Some T => T
+                   | None => Empty_set
+                   end ->
+                   option bool
+      with
+      | eq_refl => @sym_eqb _ _ _
+      end (H _) (value m f1) (value _ f2)
+    | right _ => None
+    end.
+
+  Instance RSymOneOf (m : pmap Type)
+    (H : forall p, RSym (match pmap_lookup' m p with
+                         | Some T => T
+                         | None => Empty_set
+                         end))
+  : RSym (OneOf m) :=
+  { typeof_sym s := typeof_sym_OneOf H s
+  ; symD f := symD_OneOf H f
+  ; sym_eqb f1 f2 := sym_eqb_OneOf H f1 f2
+  }.
+
+  Instance RSym_Empty : RSym (OneOf Empty) :=
+  { typeof_sym s := None
+  ; symD := fun f => match OneOf_Empty f return unit with end
+  ; sym_eqb := fun f _ => match OneOf_Empty f return option bool with end
+  }.
+
+  Instance RSymOk_OneOf (m : pmap Type)
+           (H1 : forall p, RSym (match pmap_lookup' m p with
+                                 | Some T => T
+                                 | None => Empty_set
+                                 end))
+           (H2 : forall p, RSymOk (H1 p))
+  : RSymOk (RSymOneOf m H1) :=
+  { sym_eqbOk f1 f2 :=
+      match f1 as f1'
+            return match sym_eqb f1' f2 with
+                   | Some true => f1' = f2
+                   | Some false => f1' <> f2
+                   | None => True
+                   end
+      with
+      | Build_OneOf _ x1 v1 =>
+          match f2 as f2'
+                return
+                match sym_eqb (Build_OneOf _ x1 v1) f2' with
+                | Some true => (Build_OneOf _ x1 v1) = f2'
+                | Some false => (Build_OneOf _ x1 v1) <> f2'
+                | None => True
+                end
+          with
+          | Build_OneOf _ x2 v2 => _
+          end
+      end
+  }.
+  simpl. unfold sym_eqb_OneOf. simpl.
+  destruct (Pos.eq_dec x1 x2).
+  { destruct e.
+    generalize (@sym_eqbOk _ _ _ _ (H2 x1) v1 v2).
+    destruct (sym_eqb v1 v2); auto.
+    { destruct b; intros; subst; auto.
+      intro. admit. } }
+  { trivial. }
+  Admitted.
+
+End RSym_OneOf.
