a058c5047e276ee9a64aa86f8c146814ed680bdb
jesper-bengtson <jebe@itu.dk>
2015-07-23 13:25:41 -0700

    Added patterns to NatView

--- theories/Views/NatView.v
+++ theories/Views/NatView.v
@@ -1,3 +1,6 @@
+Add Rec LoadPath "/Users/jebe/git/coq-ext-lib/theories" as ExtLib.
+Add Rec LoadPath "/Users/jebe/git/mirror-core/theories" as MirrorCore.
+
 Require Import ExtLib.Core.RelDec.
 Require Import ExtLib.Data.Fun.
 Require Import ExtLib.Data.Nat.
@@ -13,7 +16,6 @@ Require Import MirrorCore.syms.SymSum.
 Require Import MirrorCore.Lambda.Expr.
 Require Import MirrorCore.Lambda.Ptrns.
 Require Import MirrorCore.Views.FuncView.
-Require Import MirrorCore.Views.TrmD.
 Require Import MirrorCore.Views.Ptrns.
 
 Set Implicit Arguments.
@@ -55,19 +57,19 @@ Section NatFuncInst.
   end.
 	
   Definition natR (n : nat) : typD tyNat :=
-    trmR n (typ0_cast (Typ0 := Typ0_tyNat)).
+    castR id nat n.
 
   Definition natD (n : typD tyNat) : nat :=
-    trmD n (typ0_cast (Typ0 := Typ0_tyNat)).
+    castD id nat n.
 
-  Definition plusD : typD (tyArr tyNat (tyArr tyNat tyNat)) :=
-    tyArrR2 (fun a b => natR ((natD a) + (natD b))).
+  Definition plusR : typD (tyArr tyNat (tyArr tyNat tyNat)) :=
+    castR id (Fun nat (Fun nat nat)) plus.
 
-  Definition minusD : typD (tyArr tyNat (tyArr tyNat tyNat)) :=
-    tyArrR2 (fun a b => natR ((natD a) - (natD b))).
+  Definition minusR : typD (tyArr tyNat (tyArr tyNat tyNat)) :=
+    castR id (Fun nat (Fun nat nat)) minus.
 
-  Definition multD : typD (tyArr tyNat (tyArr tyNat tyNat)) :=
-    tyArrR2 (fun a b => natR ((natD a) * (natD b))).
+  Definition multR : typD (tyArr tyNat (tyArr tyNat tyNat)) :=
+    castR id (Fun nat (Fun nat nat)) mult.
 
   Definition base_func_symD bf :=
     match bf as bf return match typeofNatFunc bf return Type with
@@ -75,9 +77,9 @@ Section NatFuncInst.
 			    | None => unit
 			  end with
       | pNat n => natR n
-      | pPlus => plusD
-      | pMinus => minusD
-      | pMult => multD
+      | pPlus => plusR
+      | pMinus => minusR
+      | pMult => multR
     end.
   
   Global Instance RSym_NatFunc
@@ -111,50 +113,160 @@ Section MakeNat.
   Definition mkMinus (m n : expr typ func) := App (App (Inj fMinus) m) n.
   Definition mkMult (m n : expr typ func) := App (App (Inj fMult) m) n.
 
-  Definition fptrnNat : ptrn func nat :=
-    fun f _ good bad => 
-    match f_view f with
-      | Some (pNat n) => good n
-      | _ => bad f
-    end.
+  Definition fptrnNat {T : Type} (p : Ptrns.ptrn nat T) : ptrn natFunc T :=
+    fun f U good bad =>
+      match f with
+        | pNat n => p n U good (fun x => bad f)
+        | _ => bad f
+      end.
 
-  Definition ptrnNat : ptrn (expr typ func) nat :=
-    inj fptrnNat.
+  Definition fptrnPlus : ptrn natFunc unit :=
+    fun f U good bad =>
+      match f with
+        | pPlus => good tt
+        | _ => bad f
+      end.
 
-  Definition fptrnPlus : ptrn func unit :=
-    fun f _T good bad => 
-    match f_view f with
-      | Some pPlus => good tt
-      | _ => bad f
-    end.
+  Definition fptrnMinus : ptrn natFunc unit :=
+    fun f U good bad =>
+      match f with
+        | pMinus => good tt
+        | _ => bad f
+      end.
 
-  Definition ptrnPlus {A B : Type} 
-             (a : ptrn (expr typ func) A)
+  Definition fptrnMult : ptrn natFunc unit :=
+    fun f U good bad =>
+      match f with
+        | pMult => good tt
+        | _ => bad f
+      end.
+
+  Global Instance fptrnNat_ok {T : Type} {p : ptrn nat T} {Hok : ptrn_ok p} :
+    ptrn_ok (fptrnNat p).
+  Proof.
+    red; intros.
+    destruct x; simpl; [destruct (Hok n) | | |].
+    { left. destruct H; exists x. revert H. compute; intros.
+      rewrite H. reflexivity. }
+    { right; unfold Fails in *; intros; simpl; rewrite H; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+  Qed.
+
+  Global Instance fptrnPlus_ok : ptrn_ok fptrnPlus.
+  Proof.
+    red; intros.
+    destruct x; simpl.
+    { right; unfold Fails; reflexivity. }
+    { left; exists tt; compute; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+  Qed.
+
+  Global Instance fptrnMinus_ok : ptrn_ok fptrnMinus.
+  Proof.
+    red; intros.
+    destruct x; simpl.
+    { right; unfold Fails; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+    { left; exists tt; compute; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+  Qed.
+
+  Global Instance fptrnMult_ok : ptrn_ok fptrnMult.
+  Proof.
+    red; intros.
+    destruct x; simpl.
+    { right; unfold Fails; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+    { right; unfold Fails; reflexivity. }
+    { left; exists tt; compute; reflexivity. }
+  Qed.
+
+  Definition ptrnNat {T : Type} (p : ptrn nat T) : ptrn (expr typ func) T :=
+    inj (ptrn_view _ (fptrnNat p)).
+
+  Definition ptrnPlus {A B T : Type}
+             (a : ptrn (expr typ func) A) 
              (b : ptrn (expr typ func) B) : ptrn (expr typ func) (A * B) :=
-    pmap (fun x => match x with | (_, a, b) => (a, b) end) (app (app (inj fptrnPlus) a) b).
+    pmap (fun xy => match xy with (_, a, b) => (a, b) end) 
+         (app (app (inj (ptrn_view _ fptrnPlus)) a) b).
 
-  Definition fptrnMinus : ptrn func unit :=
-    fun f _T good bad => 
-    match f_view f with
-      | Some pPlus => good tt
-      | _ => bad f
-    end.
-
-  Definition ptrnMinus {A B : Type} 
-             (a : ptrn (expr typ func) A)
+  Definition ptrnMinus {A B T : Type}
+             (a : ptrn (expr typ func) A) 
              (b : ptrn (expr typ func) B) : ptrn (expr typ func) (A * B) :=
-    pmap (fun x => match x with | (_, a, b) => (a, b) end) (app (app (inj fptrnMinus) a) b).
+    pmap (fun xy => match xy with (_, a, b) => (a, b) end) 
+         (app (app (inj (ptrn_view _ fptrnMinus)) a) b).
 
-  Definition fptrnMult : ptrn func unit :=
-    fun f _T good bad => 
-    match f_view f with
-      | Some pPlus => good tt
-      | _ => bad f
-    end.
-
-  Definition ptrnMult {A B : Type} 
-             (a : ptrn (expr typ func) A)
+  Definition ptrnMult {A B T : Type}
+             (a : ptrn (expr typ func) A) 
              (b : ptrn (expr typ func) B) : ptrn (expr typ func) (A * B) :=
-    pmap (fun x => match x with | (_, a, b) => (a, b) end) (app (app (inj fptrnMult) a) b).
+    pmap (fun xy => match xy with (_, a, b) => (a, b) end) 
+         (app (app (inj (ptrn_view _ fptrnMult)) a) b).
+
+  Lemma Succeeds_fptrnNat {T : Type} (f : natFunc) (p : ptrn nat T) (res : T)
+        {pok : ptrn_ok p} (H : Succeeds f (fptrnNat p) res) :
+    exists n, Succeeds n p res /\ f = pNat n.
+  Proof.
+    unfold Succeeds, fptrnNat in H.
+    unfold ptrn_ok in pok.
+    specialize (H (option T) Some (fun _ => None)).
+    destruct f; try congruence.
+    specialize (pok n).
+    destruct pok; [|rewrite H0 in H; congruence].
+    destruct H0.
+    rewrite H0 in H; inv_all; subst.
+    exists n; split; [assumption | reflexivity].
+  Qed.
+
+  Lemma Succeeds_fptrnPlus (f : natFunc) (res : unit) (H : Succeeds f fptrnPlus res) :
+    f = pPlus /\ res = tt.
+  Proof.
+    split; [|destruct res; reflexivity].
+    specialize (H bool (fun _ => true) (fun _ => false)).
+    destruct f; simpl in H; try congruence.
+  Qed.
+
+  Lemma Succeeds_fptrnMinus (f : natFunc) (res : unit) (H : Succeeds f fptrnMinus res) :
+    f = pMinus /\ res = tt.
+  Proof.
+    split; [|destruct res; reflexivity].
+    specialize (H bool (fun _ => true) (fun _ => false)).
+    destruct f; simpl in H; try congruence.
+  Qed.
+
+  Lemma Succeeds_fptrnMult (f : natFunc) (res : unit) (H : Succeeds f fptrnMult res) :
+    f = pMult /\ res = tt.
+  Proof.
+    split; [|destruct res; reflexivity].
+    specialize (H bool (fun _ => true) (fun _ => false)).
+    destruct f; simpl in H; try congruence.
+  Qed.
+
+  Global Instance fptrnNat_SucceedsE {T : Type} {f : natFunc} 
+         {p : ptrn nat T} {res : T} {pok : ptrn_ok p} :
+    SucceedsE f (fptrnNat p) res := {
+      s_result := exists n, Succeeds n p res /\ f = pNat n;
+      s_elim := @Succeeds_fptrnNat T f p res pok
+    }.
+
+  Global Instance fptrnPlus_SucceedsE {f : natFunc} (res : unit) :
+    SucceedsE f fptrnPlus res := {
+      s_result := f = pPlus /\ res = tt;
+      s_elim := @Succeeds_fptrnPlus f res
+    }.
+
+  Global Instance fptrnMinus_SucceedsE {f : natFunc} (res : unit) :
+    SucceedsE f fptrnMinus res := {
+      s_result := f = pMinus /\ res = tt;
+      s_elim := @Succeeds_fptrnMinus f res
+    }.
+
+  Global Instance fptrnMult_SucceedsE {f : natFunc} (res : unit) :
+    SucceedsE f fptrnMult res := {
+      s_result := f = pMult /\ res = tt;
+      s_elim := @Succeeds_fptrnMult f res
+    }.
 
 End MakeNat.
