43476e4def1671b995316a2ea83e48e03ff2d44c
jesper-bengtson <jebe@itu.dk>
2015-07-23 10:27:35 -0700

    Removed local load paths

--- theories/Views/FuncView.v
+++ theories/Views/FuncView.v
@@ -1,6 +1,3 @@
-Add Rec LoadPath "/Users/jebe/git/coq-ext-lib/theories" as ExtLib.
-Add Rec LoadPath "/Users/jebe/git/mirror-core/theories" as MirrorCore.
-
 Require Import ExtLib.Data.Map.FMapPositive.
 Require Import ExtLib.Data.SumN.
 Require Import ExtLib.Data.Positive.
