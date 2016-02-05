dcd1ab21119757a7961f425021f721a4666b2780
Gregory Malecha <gmalecha@gmail.com>
2016-02-05 09:32:53 -0800

    there is no decidable equality for types.
    - Adding Rtype to be the 'canonical' (heterogeneous) relation on types.

--- theories/Poly/PolyP_unify.v
+++ theories/Poly/PolyP_unify.v
@@ -1,7 +1,82 @@
 Require Import ExtLib.Data.HList.
 Require Import MirrorCore.Poly.PolyPHOAS.
 
-(** UNIFICATION **)
+Section Rtype.
+  Context {Fu Fu' : list kind -> kind -> Type}.
+  Context {F F' : kind -> Type}.
+  Variable Rfu : forall ks k, Fu ks k -> Fu' ks k -> Prop.
+  Variable Rf : forall k, F k -> F' k -> Prop.
+
+  Inductive Rtype : forall k, type Fu F k -> type Fu' F' k -> Prop :=
+  | RsmVar : forall k v v', Rf k v v' -> Rtype k (smVar v) (smVar v')
+  | RsmUVar : forall ks k u u' xs xs',
+      Rfu ks k u u' -> True ->
+      Rtype _ (smUVar u xs) (smUVar u' xs')
+  | RsmArr : forall l r l' r',
+      Rtype _ l l' -> Rtype _ r r' -> Rtype _ (smArr l r) (smArr l' r')
+  | RsmAbs : forall k1 k2 t t',
+      (forall (a : F k1) (b : F' k1), Rf k1 a b -> Rtype k2 (t a) (t' b)) ->
+      Rtype _ (smAbs t) (smAbs t')
+  | RsmApp : forall k1 k2 f x f' x',
+      Rtype _ f f' -> Rtype k1 x x' ->
+      Rtype k2 (smApp f x) (smApp f' x').
+End Rtype.
+
+Definition kind_eq_dec : forall a b : kind, {a = b} + {a <> b}.
+Proof. decide equality. Defined.
+
+Section type_eq_odec.
+  Context {Fu : list kind -> kind -> Type}.
+  Context {F : kind -> Type}.
+
+  Variable u_odec : forall {ks k} (a b : Fu ks k), option (a = b).
+  Variable v_odec : forall {k} (a b : F k), option (a = b).
+
+  Fixpoint type_eq_odec {k} (a : type Fu F k) {struct a}
+  : forall b : type Fu F k, option (a = b).
+    refine
+      match a as a in type _ _ k
+            return forall b : type Fu F k, option (a = b)
+      with
+      | @smVar _ _ k v => fun b : type Fu F k =>
+        match b as b in type _ _ k
+              return forall v : F k, option (smVar v = b)
+        with
+        | smVar v' => fun v =>
+          match v_odec _ v v' with
+          | Some pf => Some (f_equal _ pf)
+          | None => None
+          end
+        | _ => fun _ => None
+        end v
+      | smUVar _ _ => fun _ => None
+      | smArr l r => fun b =>
+        match b as b in type _ _ k'
+              return option (match k' as k' return type Fu F k' -> Prop with
+                             | kStar => fun b => smArr l r = b
+                             | _ => fun _ => False
+                             end b)
+        with
+        | smArr l' r' =>
+          match type_eq_odec _ l l'
+              , type_eq_odec _ r r'
+          with
+          | Some pf , Some pf' => Some match pf in _ = X
+                                           , pf' in _ = Y
+                                             return smArr _ _ = smArr _ _ with
+                                       | eq_refl , eq_refl => eq_refl
+                                       end
+          | _ , _ => None
+          end
+        | _ => None
+        end
+      | smAbs _ => fun b => None (* Not implementable *)
+      | smApp _ _ => fun b => None
+      end.
+  Defined.
+End type_eq_odec.
+
+(** * Unification **)
 Section unify_type.
 
 (** With this representation, I lose the fact that Fu determines the list and the kind **)
@@ -11,19 +86,12 @@ Let F : kind -> Type := fun _ => nat.
 Definition TSubst : Type :=
   forall ks k, Fu ks k -> option (hlist (type Fu F) ks -> type Fu F k).
 
-
-Definition kind_eq_dec : forall a b : kind, {a = b} + {a <> b}.
-Proof. decide equality. Defined.
-
-Definition type_eq_dec : forall {k} (a b : type Fu F k), {a = b} + {a <> b}.
-Proof. Admitted.
-
 Definition TSubst_add {ks k} (u : Fu ks k) (val : hlist (type Fu F) ks -> type Fu F k) (s : TSubst) : TSubst :=
   fun a b x =>
     match PeanoNat.Nat.eq_dec u x with
     | left _ =>
       match List.list_eq_dec kind_eq_dec ks a
-            , kind_eq_dec k b
+          , kind_eq_dec k b
       with
       | left pf , left pf' =>
         Some match pf in _ = X , pf' in _ = Y
@@ -36,6 +104,12 @@ Definition TSubst_add {ks k} (u : Fu ks k) (val : hlist (type Fu F) ks -> type F
     | right _ => s a b x
     end.
 
+Definition F_eq_odec (k : kind) (a b : F k) : option (a = b) :=
+  match PeanoNat.Nat.eq_dec a b with
+  | left pf => Some pf
+  | right _ => None
+  end.
+
 Fixpoint find_in_list {ks k} (x : type Fu F k) (ls : hlist (type Fu F) ks)
 : option (hlist (type Fu F) ks -> type Fu F k) :=
   match ls in hlist _ ks
@@ -45,13 +119,14 @@ Fixpoint find_in_list {ks k} (x : type Fu F k) (ls : hlist (type Fu F) ks)
   | @Hcons _ _ k' _ l ls =>
     match kind_eq_dec k' k with
     | left pf =>
-      match @type_eq_dec k x match pf with
-                             | eq_refl => l
-                             end with
-      | left _ => Some (fun xs => match pf with
+      match type_eq_odec F_eq_odec x match pf with
+                                     | eq_refl => l
+                                     end
+      with
+      | Some _ => Some (fun xs => match pf with
                                   | eq_refl => hlist_hd xs
                                   end)
-      | right _ =>
+      | None =>
         match find_in_list x ls with
         | None => None
         | Some v => Some (fun x => v (hlist_tl x))
