f922b8d7dc721ec4e184319c0288d441026b8818
Gregory Malecha <gmalecha@gmail.com>
2016-04-14 12:02:00 +0200

    initial implementation of generic polymorphic instantiation.

---
+++ theories/Lambda/PolyInst.v
@@ -0,0 +1,86 @@
+Require Import ExtLib.Core.RelDec.
+Require Import ExtLib.Data.Map.FMapPositive.
+Require Import ExtLib.Data.Vector.
+Require Import MirrorCore.TypesI.
+Require Import MirrorCore.SymI.
+Require Import MirrorCore.Lambda.ExprCore.
+Require Import MirrorCore.Views.View.
+
+Set Implicit Arguments.
+Set Strict Implicit.
+
+Section poly.
+  Context {typ : Type} {sym : Type}.
+  Context {RT : RType typ}
+          {RS : RSym sym}.
+
+  Inductive VType : Type :=
+  | tVar : positive -> VType.
+
+  Variable typ_unify : typ -> typ -> pmap typ -> option (pmap typ).
+
+  Context {PV_vtype : PartialView typ VType}.
+
+  (** NOTE: This function does not need to be complete **)
+  Fixpoint get_types {T} (a b : expr typ sym) (s : pmap typ)
+           (ok : pmap typ -> T) (bad : T) {struct a}
+  : T.
+  refine
+    match a , b with
+    | App fa aa , App fb ab =>
+      get_types _ fa fb s
+                 (fun s' => get_types _ aa ab s' ok bad)
+                 bad
+    | Inj a , Inj b =>
+      match typeof_sym a
+          , typeof_sym b
+      with
+      | Some ta , Some tb =>
+        match typ_unify ta tb s with
+        | Some s' => ok s'
+        | None => bad
+        end
+      | _ , _ => bad
+      end
+    | _ , _ => ok s
+    end.
+  Defined.
+
+  Fixpoint polymorphic (n : nat) T : Type :=
+    match n with
+    | 0 => T
+    | S n => typ -> polymorphic n T
+    end.
+
+  Fixpoint inst {T} (n : nat)
+  : polymorphic n T -> vector typ n -> T :=
+    match n as n return polymorphic n T -> vector typ n -> T with
+    | 0 => fun p _ => p
+    | S n => fun p a => inst (p (Vector.vector_hd a)) (Vector.vector_tl a)
+    end.
+
+  Fixpoint build_vector p (n : nat) : vector typ n :=
+    match n with
+    | 0 => Vnil _
+    | S n => Vcons (f_insert (tVar p)) (build_vector (Pos.succ p) n)
+    end.
+
+  Fixpoint get_vector {T} n p
+  : forall (ok : vector typ n -> T) (bad : T) (m : pmap typ), T :=
+    match n as n return (vector typ n -> T) -> T -> pmap typ -> T with
+    | 0 => fun ok _ _ => ok (Vnil _)
+    | S n => fun ok bad m =>
+               match pmap_lookup p m with
+               | None => bad
+               | Some z => get_vector (Pos.succ p)
+                                      (fun vs => ok (Vcons z vs)) bad m
+               end
+    end.
+
+  Definition get_inst {n} (t : polymorphic n (expr typ sym)) (w : expr typ sym)
+  : option (vector typ n) :=
+    let t' := inst t (build_vector 1 n) in
+    get_types t' w (pmap_empty _)
+              (get_vector 1 Some None)
+              None.
+End poly.
---
+++ theories/MTypes/MTypeUnify.v
@@ -0,0 +1,56 @@
+Require Import MirrorCore.MTypes.ModularTypes.
+
+Section parametric.
+  Variable tsym : nat -> Type.
+  Variable TSym_tsym : TSym tsym.
+
+  Variable S : Type.
+  Variable add : tsym 0 -> mtyp tsym -> S -> option S.
+
+  (** This is asymmetric, the first argument is the "special one" **)
+  Fixpoint mtype_unify (a b : mtyp tsym) (s : S) {struct a}
+  : option S :=
+    match a with
+    | tyArr _ da ca =>
+      match b with
+      | tyArr _ db cb =>
+        match mtype_unify da ca s with
+        | Some s' => mtype_unify ca cb s'
+        | _ => None
+        end
+      | _ => None
+      end
+    | tyBase0 sym => add sym b s
+    | tyBase1 _ ta =>
+      match b with
+      | tyBase1 _ tb => mtype_unify ta tb s
+      | _ => None
+      end
+    | tyBase2 _ ta ta' =>
+      match b with
+      | tyBase2 _ tb tb' =>
+        match mtype_unify ta tb s with
+        | Some s' => mtype_unify ta' tb' s'
+        | _ => None
+        end
+      | _ => None
+      end
+    | tyApp _ va =>
+      match b with
+      | tyApp _ vb =>
+        (fix go n n' (va : Vector.vector _ n) (vb : Vector.vector _ n') (s : S)
+         : option S :=
+           match va , vb with
+           | Vector.Vnil _ , Vector.Vnil _ => Some s
+           | Vector.Vcons a va , Vector.Vcons b vb =>
+             match mtype_unify a b s with
+             | Some s' => go _ _ va vb s'
+             | _ => None
+             end
+           | _ , _ => None
+           end) _ _ va vb s
+      | _ => None
+      end
+    end.
+
+End parametric.
