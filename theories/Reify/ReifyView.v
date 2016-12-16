386b22ea33136c008dce7cb2585d3e21b60cad41
jesper-bengtson <jebe@itu.dk>
2016-12-16 23:40:38 +0100

    Reverted ReifyView

--- theories/Reify/ReifyView.v
+++ theories/Reify/ReifyView.v
@@ -49,26 +49,19 @@ Section ReifyFunc.
   Polymorphic Definition reify_func_aux 
               (rf_pre rf_post : 
                  list (Command (expr typ func))) : Command (expr typ func) :=
-    @Patterns.CFirst_ _
-      (rf_pre ++ ((Patterns.CVar (@ExprCore.Var typ func)) ::
-         (Patterns.CApp (Patterns.CRec 0) (Patterns.CRec 0) (@ExprCore.App typ func)) ::
-	 (Patterns.CAbs (CCall (reify_scheme typ)) (CRec 0) (@ExprCore.Abs typ func)) ::
-	  rf_post)).
+    @Patterns.CFix (expr typ func)
+      (@Patterns.CFirst_ _
+        (rf_pre ++ ((Patterns.CVar (@ExprCore.Var typ func)) ::
+           (Patterns.CApp (Patterns.CRec 0) (Patterns.CRec 0) (@ExprCore.App typ func)) ::
+	   (Patterns.CAbs (CCall (reify_scheme typ)) (CRec 0) (@ExprCore.Abs typ func)) ::
+	    rf_post))).
 
   Polymorphic Definition reify_func
               (rf : list (Command (expr typ func))) : Command (expr typ func) :=
-    @Patterns.CFix (expr typ func)
-      (reify_func_aux rf (Patterns.CMap Ext (Patterns.CTypedTable (reify_scheme typ) term_table)::nil)).
+    reify_func_aux rf (Patterns.CMap Ext (Patterns.CTypedTable (reify_scheme typ) term_table)::nil).
 
   Polymorphic Definition reify_func_no_table
               (rf : list (Command (expr typ func))) : Command (expr typ func) :=
-    @Patterns.CFix (expr typ func) (reify_func_aux rf nil).
-
-  Polymorphic Definition reify_func_no_fix 
-              (rf : list (Command (expr typ func))) : Command (expr typ func) :=
-      reify_func_aux rf (Patterns.CMap Ext (Patterns.CTypedTable (reify_scheme typ) term_table)::nil).
-
-  Polymorphic Definition reify_func_no_table_no_fix (rf : list (Command (expr typ func))) : Command (expr typ func) :=
     reify_func_aux rf nil.
 
   Polymorphic Instance Reify_func (rf : list (Command (expr typ func))) : Reify (expr typ func) := {
@@ -79,24 +72,10 @@ Section ReifyFunc.
     reify_scheme := reify_func_no_table rf
   }.
 
-  Polymorphic Instance Reify_func_no_fix (rf : list (Command (expr typ func))) : Reify (expr typ func) := {
-    reify_scheme := reify_func_no_fix rf
-  }.
-
-  Polymorphic Instance Reify_func_no_table_no_fix (rf : list (Command (expr typ func))) : Reify (expr typ func) := {
-    reify_scheme := reify_func_no_table_no_fix rf
-  }.
-
 End ReifyFunc.
 
 Arguments reify_func _ _ {_ _} _.
 Arguments Reify_func _ _ {_ _} _.
 
-Arguments reify_func_no_fix _ _ {_ _} _.
-Arguments Reify_func_no_fix _ _ {_ _} _.
-
 Arguments reify_func_no_table _ _ {_} _.
 Arguments Reify_func_no_table _ _ {_} _.
-
-Arguments reify_func_no_table_no_fix _ _ {_} _.
-Arguments Reify_func_no_table_no_fix _ _ {_} _.
